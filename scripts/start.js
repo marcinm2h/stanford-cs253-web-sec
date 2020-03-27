947ca974ec42bfc6eb8ad1b83631b9cf72e43283
Marcin Moch <marcio.moch@gmail.com>
2020-03-27 23:31:54 +0100

    Fixed start script

--- scripts/start.js
+++ scripts/start.js
@@ -1,19 +1,37 @@
+const path = require("path");
+const { readdirSync } = require("fs");
+const { exec } = require("child_process");
+
 const [_, __, exercise, ...args] = process.argv;
 
 if (!exercise) {
-  throw new Error("Pass exercise number as an argument (e.g. node start.js 4");
+  throw new Error(
+    "Pass exercise directory name as an argument (e.g. node start.js 04-cors"
+  );
 }
 
-const exercises = {
-  '4': () => {
-    console.log('TODO');
-  }
-};
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
 
-if (!(exercise in exercises)) {
-  throw new Error("Invalid exercise number");
+const exercises = getDirectories(path.resolve(".")).filter(
+  dir => ![".git", "node_modules", "scripts"].includes(dir)
+);
+
+if (!exercises.includes(exercise)) {
+  throw new Error("Invalid exercise name");
 }
+
+const startScriptPath = require.resolve(
+  path.join(__dirname, "..", exercise, "start.sh")
+);
 
 console.log(`Exercise ${exercise}`);
+const childProcess = exec(`sh ${startScriptPath}`);
+childProcess.stdout.pipe(process.stdout);
 
-exercises[exercise](...args);
+process.on("SIGINT", () => {
+  childProcess.kill("SIGINT");
+  process.exit();
+});

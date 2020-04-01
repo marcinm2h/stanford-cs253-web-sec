9ab098bc328620fcb97ef9ddc8fc088bf921a58c
Marcin Moch <marcio.moch@gmail.com>
2020-04-01 23:46:49 +0200

    CSP script-dynamic

--- src/07-ejs-template-lang/app.js
+++ src/07-ejs-template-lang/app.js
@@ -10,6 +10,26 @@ app.get("/", (req, res) => {
   const template = `
     <h1>Hi, (%=) <%= name %></h1>
     <h1>Hi, (%-) <%- name %></h1>
+    <a href="?name=<script>alert('xss')</script>">XSS</a>
+    <br />
+    <a href="/csp?name=<script>alert('xss')</script>">'Content-Security-Policy': "default-src 'self'"</a>
+    `;
+  const html = ejs.render(template, { name });
+
+  res.send(html);
+});
+
+app.get("/csp", (req, res) => {
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src
+  res.set({
+    'Content-Security-Policy': "default-src 'self'" // prevents external <src /> and in-line <src />
+  });
+
+  const { name = "guest" } = req.query;
+  const template = `
+    <h1>Hi, (%=) <%= name %></h1>
+    <h1>Hi, (%-) <%- name %></h1>
+    <a href="?name=<script>alert('xss')</script>">XSS</a>
     `;
   const html = ejs.render(template, { name });
 
